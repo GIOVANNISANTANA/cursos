8df5487ad308a2b6fc8ccb05451d4d2e2f413d24
Giovanni Santana <GIOVANNIALMEIDA11@HOTMAIL.COM>
2021-04-21 21:53:06 -0300

    Criação dos exercicios 008 e 009
    Criação dos exercicios 008 e 009 referente ao tema condições em JS

--- javascript/aula11/ex008.js
+++ javascript/aula11/ex008.js
@@ -1,1 +1,6 @@
-console.log('teste!')
+var vel = 60.0
+console.log(`À velocidade do seu carro é ${vel}km/h`)
+if (vel > 60){ //condição simples, só tem uma condição
+    console.log(`Você está multado`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
